9d55e0d2004f75753a1f0c12aac0fcb67d263e90
Aleksandre <amamismeda@gmail.com>
2023-05-22 12:18:51 +0400

    made existing budgets cards

--- src/helpers.js
+++ src/helpers.js
@@ -49,3 +49,40 @@ export const createExpense = ({ name, amount, budgetId }) => {
 export const deleteItem = ({ key }) => {
   return localStorage.removeItem(key);
 };
+
+
+//total spent by budget
+
+export const calculateSpentByBudget = (budgetId) => {
+  const expenses = fetchData("expenses") ?? [];
+  const budgetSpent = expenses.reduce((acc, expense) => {
+    //check if expense.id === budget Id i passed in
+    if(expense.budgetId != budgetId) return acc
+
+    //add the current amount to my total
+    return acc += expense.amount
+  }, 0)
+  return budgetSpent;
+}
+
+
+//formating
+
+
+//formatting percentage
+export const formatPercentage = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "percent",
+    minimumFractionDigits: 0
+  })
+}
+
+
+/// format currency
+
+export const formatCurrency = (amt) => {
+  return amt.toLocaleString(undefined, {
+    style: "currency",
+    currency: "USD"
+  })
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,6 +3,7 @@ import { useLoaderData } from "react-router-dom";
 import Intro from "../components/Intro";
 import AddBudgetForm from "../components/AddBudgetForm";
 import AddExpenseForm from "../components/AddExpenseForm";
+import BudgetItem from "../components/BudgetItem";
 
 //helper functions
 import { createBudget, createExpense, fetchData, waait } from "../helpers";
@@ -50,10 +51,10 @@ export async function dashboardAction({ request }) {
       //create expense
       createExpense({
         name: values.newExpense,
-        amount : values.newExpenseAmount,
-        budgetId: values.newExpenseBudget
-      })
-     
+        amount: values.newExpenseAmount,
+        budgetId: values.newExpenseBudget,
+      });
+
       return toast.success(`Expense ${values.newExpense} created!`);
     } catch (error) {
       throw new Error("There was a problem creating your budget.");
@@ -72,25 +73,26 @@ const Dashboard = () => {
             Welcome back, <span className="accent">{userName}</span>
           </h1>
           <div className="grid-sm">
-            {
-              budgets && budgets.length > 0
-              ? (
+            {budgets && budgets.length > 0 ? (
               <div className="grid-lg">
                 <div className="flex-lg">
                   <AddBudgetForm />
                   <AddExpenseForm budgets={budgets} />
                 </div>
-              </div>
-              )
-              :
-              (
-                <div className="grid-sm">
-                  <p>Personal budgeting is the secret to financial freedom</p>
-                  <p>Create a budget to get started !</p>
-                  <AddBudgetForm />
+                <h2>Existing Budgets</h2>
+                <div className="budgets">
+                  {budgets.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                  ))}
                 </div>
-              )
-            }
+              </div>
+            ) : (
+              <div className="grid-sm">
+                <p>Personal budgeting is the secret to financial freedom</p>
+                <p>Create a budget to get started !</p>
+                <AddBudgetForm />
+              </div>
+            )}
           </div>
         </div>
       ) : (
